47b0261bcbf38d1e110b31b16b415d8af28bbebb
Pawel Laciak <laciakp1@gmail.com>
2023-06-04 13:16:39 -0500

    initial

---
+++ src/test/java/com/revature/beerme/BeermeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.beerme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeermeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
